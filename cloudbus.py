6b0103f5ed15162af4da1ab72cbeb8334941422e
rmay-intwine <rmay@intwineconnect.com>
2017-08-03 13:14:53 -0400

    Add files via upload

---
+++ cloudbus.py
@@ -0,0 +1,85 @@
+################################################################################
+# Copyright (c) 2017                                                           #
+# Intwine Connect, LLC.                                                        #
+#                                                                              #
+# BSD-2-Clause                                                                 #
+# DESCRIPTION OF OTHER RIGHTS AND LIMITATIONS                                  #
+# Redistribution and use in source and binary forms, with or without           #
+# modification, are permitted provided that the following conditions are met:  #
+# 1. Redistributions of source code must retain the above copyright notice,    #
+#    this list of conditions and the following disclaimer.                     #
+# 2. Redistributions in binary form must reproduce the above copyright notice, #
+#    this list of conditions and the following disclaimer in the documentation #
+#    and/or other materials provided with the distribution.                    #
+#                                                                              #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" #
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,        #
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR       #
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR            #
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,        #
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,          #
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;  #
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,     #
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR      #
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF       #
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                                   #
+################################################################################
+
+import urllib
+import json
+import datetime as dt
+
+class cbDevice():
+    """CloudBUS Device Class
+
+    """
+    guid = None
+    data = {}
+
+    def __init__(self, guid=None):
+        if guid is not None:
+            self.guid = guid
+
+    def setGUID(self, guid):
+        """Assigns the cbDevice a specified guid
+
+        Args:
+            guid: the string representation of the device identifier
+        """
+
+        assert guid is True, "GUID can not be empty"
+        self.__init__(guid)
+
+    def getData(self, variable, tstart=None, tend=None):
+        """Get data from the CloudBUS device APIs
+
+        """
+        if tstart is None:
+            tstart = dt.datetime.fromtimestamp(0)
+        if tend is None:
+            tend = dt.datetime.now() + dt.timedelta(days=1)  # tomorrow
+
+        # build the CloudBUS URI
+        url = 'http://54.172.91.188:8080/cloudbus/device/'
+        query = '/data?attr=%s' % variable
+        query += '&start=' + tstart.strftime("%Y-%m-%d %H:%M:%S")
+        query += '&end=' + tend.strftime("%Y-%m-%d %H:%M:%S")
+        query = query.replace(' ', '%20')
+
+        # request the URL and read the response
+        read_url = urllib.urlopen(url + self.guid + query)
+        response = ""
+        for line in read_url:
+            response += line
+        read_url.close()
+        resp = json.loads(response)
+
+        # format the data to be returned
+        data = dict(resp['data'])
+        a = sorted(data.items())
+        t_vector = []
+        y_vector = []
+        for i in a:
+            t_vector.append( dt.datetime.fromtimestamp(float(i[0])/1000.0) )
+            y_vector.append( float(i[1]) )
+        return t_vector, y_vector
