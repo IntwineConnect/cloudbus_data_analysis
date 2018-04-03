fcab9fc01e36b79e3cc7c1eebd6720e703b21aa7
rmay-intwine <rmay@intwineconnect.com>
2018-04-03 08:30:40 -0400

    Add support for OAuth (#5)
    * Support OAuth2 on client connections to Cloudbus
    
    * Support OAuth2 on Python 3.x
    
    * Fixes to enable Python3 to use OAuth. (#4)
    
    * Fixes to enable Python3 to use OAuth.
    
    * Remove unnecessary commented code.
    
    * Move duplicated lines into one place.

--- cloudbus.py
+++ cloudbus.py
@@ -26,21 +26,61 @@
 ################################################################################
 
 import urllib
-import json
-import datetime as dt
 import sys
 
-CBUS_IP = "cbws.intwineconnect.com:8080"
+# Workaround for Python 2 vs 3
+if sys.version_info[0] == 2:
+    import urllib2
 
+import json
+import datetime as dt
+from base64 import b64encode
 
-def get_response(uri):
+#CBUS_IP = "cbws.intwineconnect.com:8080"
+CBUS_IP = '52.11.142.255:8080'
+
+# OAuth2 client information
+GET_TOKEN = "/cloudbus/oauth/token"
+clientId = ""
+apiKey = ""
+
+def get_oauth_token():
+    data = clientId + ':' + apiKey
+
+    # Handle differently for Python3
+    if sys.version_info[0] == 3:
+        data_bytes = data.encode("utf-8")
+        creds = b64encode(data_bytes)
+        creds = creds.decode("utf-8")
+        headers = {'Authorization': 'Basic ' + creds}
+        body = urllib.parse.urlencode({'grant_type': 'client_credentials'})
+    else:
+        creds = b64encode(data)
+        headers = {'Authorization': 'Basic ' + creds}
+        body = urllib.urlencode({'grant_type': 'client_credentials'})
+
+    url = "http://" + CBUS_IP + GET_TOKEN
+    response = get_response(url, data=body, headers=headers)
+
+    if 'access_token' in response:
+        return response
+    else:
+        return None
+
+def get_response(uri, data=None, headers=None):
     # Python 3 uses a different process to get the response
     if sys.version_info[0] == 3:
-        with urllib.request.urlopen(uri) as read_url:
+        req = urllib.request.Request(uri, headers=headers)
+        if data:
+            data_bytes = data.encode("utf-8")
+            req.data = data_bytes
+        with urllib.request.urlopen(req) as read_url:
             s = read_url.read()
         response = s.decode('utf-8')
     else:
-        read_url = urllib.urlopen(uri)
+        req = urllib2.Request(uri, headers=headers)
+        if data: req.add_data(data)
+        read_url = urllib2.urlopen(req)
         response = ""
         for line in read_url:
             response += line
@@ -59,10 +99,14 @@ class cbDevice():
 
     guid = None
     data = {}
+    oauth_header = None
 
     def __init__(self, guid=None):
         if guid is not None:
             self.guid = guid
+        token = get_oauth_token()
+        if token:
+            self.oauth_header = {'Authorization': 'Bearer ' + token['access_token']}
 
     def setGUID(self, guid):
         """Assigns the cbDevice a specified guid
@@ -111,7 +155,7 @@ class cbDevice():
         query = query.replace(' ', '%20')
 
         # request the URL and read the response
-        resp = get_response(url + self.guid + query)
+        resp = get_response(url + self.guid + query, headers=self.oauth_header)
 
         # format the data to be returned
         data = dict(resp['data'])
@@ -119,7 +163,7 @@ class cbDevice():
         t_vector = []
         y_vector = []
         for i in a:
-            # note that the timestamp is converted to the platform’s local date
+            # note that the timestamp is converted to the platforms local date
             # and time, and the returned datetime object is naive.
             t_vector.append( dt.datetime.fromtimestamp(float(i[0])/1000.0) )
             y_vector.append( float(i[1]) )
@@ -145,7 +189,7 @@ class cbDevice():
         url = 'http://' + CBUS_IP + '/cloudbus/device/'
         query = '/currentdata'
         # request the URL and read the response
-        resp = get_response(url + self.guid + query)
+        resp = get_response(url + self.guid + query, headers=self.oauth_header)
 
         # format the data to be returned
         current_data = {}
@@ -175,7 +219,7 @@ class cbDevice():
         # build the CloudBUS URI
         url = 'http://' + CBUS_IP + '/cloudbus/device/'
         # request the URL and read the response
-        return get_response(url + self.guid)
+        return get_response(url + self.guid, headers=self.oauth_header)
 
 class cbGateway(cbDevice):
     """CloudBUS Gateway device
@@ -197,7 +241,7 @@ class cbGateway(cbDevice):
         # build the CloudBUS URI
         url = 'http://' + CBUS_IP + '/cloudbus/gateway/'
         # request the URL and read the response
-        resp = get_response(url + self.guid)
+        resp = get_response(url + self.guid, headers=self.oauth_header)
 
         # format the data to be returned
         devices = {}
@@ -229,7 +273,7 @@ class cbGateway(cbDevice):
         url = 'http://' + CBUS_IP + '/cloudbus/device/'
         query = '/currentdata'
         # request the URL and read the response
-        resp = get_response(url + self.guid + query)
+        resp = get_response(url + self.guid + query, headers=self.oauth_header)
 
         # format the data to be returned
         current_data = {}
